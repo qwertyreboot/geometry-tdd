8f40b959e3bcf564ee85bc98d0ae56ca84a874fe
Khavin Shankar <khavinshankar@gmail.com>
2021-10-11 20:32:09 +0530

    feat: added area in Rectangle

--- src/main/java/shapes/Rectangle.java
+++ src/main/java/shapes/Rectangle.java
@@ -1,5 +1,15 @@
 package shapes;
 
 public class Rectangle {
+    private double length;
+    private double breadth;
 
+    Rectangle(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public double area() {
+        return length * breadth;
+    }
 }
