4e575e09c07f8c53b32648c36bb20accb1e84ade
Khavin Shankar <khavinshankar@gmail.com>
2021-10-11 22:09:31 +0530

    test: added test cases can to verify that the length and breadth can only be +ve

--- src/test/java/shapes/RectangleTest.java
+++ src/test/java/shapes/RectangleTest.java
@@ -1,12 +1,14 @@
 package shapes;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
     @Test
+    @DisplayName("verify by default the value of length and breadth is 1")
     public void verifyDefaultValuesOfAttributes() {
         Rectangle rectangle = new Rectangle();
         Assertions.assertEquals(rectangle.getLength(), 1);
@@ -14,6 +16,31 @@ class RectangleTest {
     }
 
     @Test
+    @DisplayName("verify that the value of length cannot be -ve or 0")
+    public void verifyLengthIsValid() {
+        Rectangle rectangle = new Rectangle();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            rectangle.setLength(Math.random() * -1);
+        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            rectangle.setLength(0);
+        });
+    }
+
+    @Test
+    @DisplayName("verify that the value of breadth cannot be -ve or 0")
+    public void verifyBreadthIsValid() {
+        Rectangle rectangle = new Rectangle();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            rectangle.setBreadth(Math.random() * -1);
+        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            rectangle.setBreadth(0);
+        });
+    }
+
+    @Test
+    @DisplayName("verify that the calculation of the area is correct")
     public void verifyAreaOutput() {
         double length = Math.random();
         double breadth = Math.random();
