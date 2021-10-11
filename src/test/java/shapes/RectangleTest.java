588c860b39d89be046885d463535f1ba49c5df64
Khavin Shankar <khavinshankar@gmail.com>
2021-10-11 22:24:09 +0530

    refactor: added setup method in RectangleTest

--- src/test/java/shapes/RectangleTest.java
+++ src/test/java/shapes/RectangleTest.java
@@ -1,16 +1,23 @@
 package shapes;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
+    private Rectangle rectangle;
+
+    @BeforeEach
+    private void setup() {
+        rectangle = new Rectangle();
+    }
+
     @Test
     @DisplayName("verify by default the value of length and breadth is 1")
     public void verifyDefaultValuesOfAttributes() {
-        Rectangle rectangle = new Rectangle();
         Assertions.assertEquals(rectangle.getLength(), 1);
         Assertions.assertEquals(rectangle.getBreadth(), 1);
     }
@@ -18,7 +25,6 @@ class RectangleTest {
     @Test
     @DisplayName("verify that the value of length cannot be -ve or 0")
     public void verifyLengthIsValid() {
-        Rectangle rectangle = new Rectangle();
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             rectangle.setLength(Math.random() * -1);
         });
@@ -30,7 +36,6 @@ class RectangleTest {
     @Test
     @DisplayName("verify that the value of breadth cannot be -ve or 0")
     public void verifyBreadthIsValid() {
-        Rectangle rectangle = new Rectangle();
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             rectangle.setBreadth(Math.random() * -1);
         });
@@ -44,7 +49,8 @@ class RectangleTest {
     public void verifyAreaOutput() {
         double length = Math.random();
         double breadth = Math.random();
-        Rectangle rectangle = new Rectangle(length, breadth);
+        rectangle.setLength(length);
+        rectangle.setBreadth(breadth);
         Assertions.assertEquals(rectangle.area(), length * breadth);
     }
 }
