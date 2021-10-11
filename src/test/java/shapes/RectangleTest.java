a435a6278e8c47da4047970ec97887adfd650ae4
Khavin Shankar <khavinshankar@gmail.com>
2021-10-11 20:40:46 +0530

    test: basic test to verify Rectangle.area()

--- src/test/java/shapes/RectangleTest.java
+++ src/test/java/shapes/RectangleTest.java
@@ -1,9 +1,14 @@
 package shapes;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
-
+    @Test
+    public void verifyAreaOutput() {
+        Rectangle rectangle = new Rectangle(5, 10);
+        Assertions.assertEquals(rectangle.area(), 50);
+    }
 }
